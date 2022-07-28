5e2deb5b09a9e02c52af72e3819b35a4952d0335
ukflava <ukflava@gmail.com>
2022-07-28 16:20:04 -0700

    render table with links to categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -27,7 +27,7 @@ class Admin::CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:product).permit(
+    params.require(:category).permit(
       :name,
       
                   
