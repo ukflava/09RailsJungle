4da9d5cae93eeca1779b2ad664f11ad2a22ebe24
ukflava <ukflava@gmail.com>
2022-07-29 07:26:07 -0700

    change routes to delete

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,8 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
+
 
   namespace :admin do
     root to: 'dashboard#show'
