0f032e334eea8af785ea22d4db155a9ed89c3a07
ukflava <ukflava@gmail.com>
2022-07-28 20:15:47 -0700

    layout ruined, but auth user works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,15 @@
 class ProductsController < ApplicationController
 
+
+  before_action :authorize
+
+  def cool
+  end
+
+  def free
+  end
+
+
   def index
     @products = Product.all.order(created_at: :desc)
   end
