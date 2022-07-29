5dc7ee4f96f19d84fde019b1a547e0581eab3c11
ukflava <ukflava@gmail.com>
2022-07-29 07:17:46 -0700

    auth add to body, tested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   
   protect_from_forgery with: :exception
-
-
-  
-  private
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 
+  
+  private
+
+
+
   def authorize
-    redirect_to '/login' unless current_user
+    redirect_to login_path, alert: 'You must be logged in to access this page.' if current_user.nil?
   end
 
   def cart
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
-class UsersController < ApplicationController
+class User < ActiveRecord::Base
   has_secure_password
-  # Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
-	validates :email, presence: true, uniqueness: true
-	
-	
- 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates_length_of :password, :minimum => 5
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_confirmation, presence: true
 
+  before_save :clean_email
+
+  def self.authenticate_with_credentials(email, password)
+    return nil if email == nil
+
+    @user = User.find_by_email(email.downcase.strip)
+    @user && @user.authenticate(password) ? @user : nil
+  end
+
+  private
+
+    def clean_email
+      self.email.downcase!
+      self.email.strip!
+    end
 
 end
