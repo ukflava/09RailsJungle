4268907ac6bee410742402c151a6fd9d4390de4e
ukflava <ukflava@gmail.com>
2022-08-01 14:16:36 -0700

    test for products with categiry

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
+  before(:all) do 
+     @category = Category.new
+
+  @category.save!
+  end
+  
   describe 'Validations' do
-    validates :name, presence: true
+    # validates :name, presence: true
+    @product = Product.new
+    # we use bang here b/c we want our spec to fail if save fails (due to validations)
+    # we are not testing for successful save so we have to assume it will be successful
+    @product.save!
+
+    expect(@product.name).to be_present
     # validation tests/examples here
   end
 end
