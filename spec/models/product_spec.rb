68a3e9c12cabfdaae30cf88c9c402ff83b980e1d
ukflava <ukflava@gmail.com>
2022-08-01 16:36:43 -0700

    test should pass if no price or name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -30,19 +30,36 @@ RSpec.describe Product, type: :model do
       expect(@product.name).to match("Giant Test 2")
     end
 
-    it 'should not save without price' do
-      @product2 = Product.new
-      @product2.name = "Giant Test 2"
-      @product2.price = nil
-      @product2.quantity = 1
-      @product2.category_id = @category.id
+    it 'should NOT save with no name' do
+      @product = Product.new
+      @product.name = nil
+      @product.price = 500
+      @product.quantity = 1
+      @product.category_id = @category.id
 
 
-      @product2.save!
+      @product.save
+      p @product.errors
 
-      expect(@product2).to be_nil
+      expect(@product).to_not be_valid
       # expect(@product.name).to match("Giant Test 2")
     end
+
+
+    # it 'should not save without price' do
+    #   @product2 = Product.new
+    #   @product2.name = "Giant Test 2"
+    #   @product2.price = nil
+    #   @product2.quantity = 1
+    #   @product2.category_id = @category.id
+
+
+    #   @product2.save!
+    #   p @product2.errors
+
+    #   expect(@product2).to be_nil
+    #   # expect(@product.name).to match("Giant Test 2")
+    # end
 
 #  Set all fields to a value but the validation field being tested to nil
 # Test that the expect error is found within the .errors.full_messages array
