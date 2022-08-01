b5e0beb61e56f842081b47c13a7334a82f7d0dd7
ukflava <ukflava@gmail.com>
2022-08-01 14:01:06 -0700

    start testing specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    
+    validates :name, presence: true
     # validation tests/examples here
   end
 end
