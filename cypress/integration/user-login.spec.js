7b843ada5920efc86a6f0af48fd5a5f02648a50a
ukflava <ukflava@gmail.com>
2022-08-02 18:36:32 -0700

    add test users

--- cypress/integration/user-login.spec.js
+++ cypress/integration/user-login.spec.js
@@ -1,5 +1,7 @@
 describe("Appointment", () => {
   beforeEach(() => {
+
+ 
     // cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     // cy.contains("You must be logged in to access this page.");
@@ -21,11 +23,12 @@ describe("Appointment", () => {
   // });
 
   it("There is products on the page", () => {
+ 
     cy.get(".products article").should("be.visible");
   });
 
 
-  it("Signup", () => {
+  it("can Signup new user", () => {
 
 
 
@@ -33,7 +36,16 @@ describe("Appointment", () => {
 
 
     cy.get('a[href*="users/new"]').click({force: true}).wait(500)
-    // cy.get('a[href*="login"]').click({force: true}).wait(500)
+    cy.get('[id=user_first_name]').type("Lydia")
+    cy.get('[id=user_last_name]').type("Miller-Jones")
+    // cy.get('[id=user_email]').type("Miller@test.com")
+    cy.get('[id=user_email]').type("Miller@test.com")
+    cy.get('[id=user_password]').type("Millertestcom")
+    cy.get('[id=user_password_confirmation]').type("Millertestcom")
+    cy.get('[id=registerButton]').click().wait(500)
+    cy.contains("Account created successfully!");
+
+
   //   cy.get(".products article").should("have.length", 2);
   // cy.findByText("Sign Up").should("exist")
   // cy.contains('Sign Up').click()
@@ -44,6 +56,28 @@ describe("Appointment", () => {
     // cy.get("[class='nav-item end-0']").click({ multiple: true });
     // cy.contains("TOTAL");
   });
+  it("Can login", () => {
+
+
+    cy.get('a[href*="login"]').click({force: true}).wait(500)
+    
+    cy.get('[id=login_email]').type("Miller@test.com")
+    cy.get('[id=login_password]').type("Millertestcom")
+        cy.get('[f.submit]').click().wait(500)
+    cy.contains("Miller@test.com");
+
+
+  //   cy.get(".products article").should("have.length", 2);
+  // cy.findByText("Sign Up").should("exist")
+  // cy.contains('Sign Up').click()
+  // cy.get('href="/users/new').click().wait(500);;
+    // cy.get("[alt='Scented Blade']").click().wait(500);
+    // cy.get("[type='submit']").click({ multiple: true }).wait(500);
+    // cy.contains("The Scented Blade is an extremely rare, tall plant and can be found mostly in savannas.");
+    // cy.get("[class='nav-item end-0']").click({ multiple: true });
+    // cy.contains("TOTAL");
+  });
+
 
  
 
